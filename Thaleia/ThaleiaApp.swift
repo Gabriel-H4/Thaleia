0a2561a80ad0f4e67e073b2fb0769669e83dc47d
Gabriel Hassebrock <gabriel.h4@icloud.com>
2025-03-29 16:58:06 -0500

    Initial Commit

---
+++ Thaleia/ThaleiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThaleiaApp.swift
+//  Thaleia
+//
+//  Created by Gabriel Hassebrock on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ThaleiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
